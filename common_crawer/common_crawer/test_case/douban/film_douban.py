337c8fffae2a57365d88244085069f402d589d3e
ximingren <987327263@qq.com>
2018-11-07 23:45:35 +0800

    2

--- common_crawer/common_crawer/test_case/douban/film_douban.py
+++ common_crawer/common_crawer/test_case/douban/film_douban.py
@@ -6,7 +6,8 @@ import pymongo
 import requests
 from lxml import etree
 from common_crawer.common_crawer.MongoQueue import MongoQueue
-CookieList=[]
+
+CookieList = []
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8',
@@ -14,7 +15,7 @@ headers = {
     'Content-Type': 'text/html;charset=UTF-8',
     'Cache-Control': 'no-cache',
     'Host': 'movie.douban.com',
-    'Pragma':'no-cache',
+    'Pragma': 'no-cache',
     'Referer': 'www.baidu.com',
     # 'Cookie': 'll="118282"; bid=ctyiEarSLfw; ps=y; __yadk_uid=0Sr85yZ9d4bEeLKhv4w3695OFOPoedzC; dbcl2="155150959:OEu4dds1G1o"; as="https://sec.douban.com/b?r=https%3A%2F%2Fbook.douban.com%2F"; ck=fTrQ; _pk_id.100001.4cf6=c86baf05e448fb8d.1506160776.3.1507290432.1507283501.; _pk_ses.100001.4cf6=*; __utma=30149280.1633528206.1506160772.1507283346.1507290433.3; __utmb=30149280.0.10.1507290433; __utmc=30149280; __utmz=30149280.1506160772.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=223695111.1475767059.1506160772.1507283346.1507290433.3; __utmb=223695111.0.10.1507290433; __utmc=223695111; __utmz=223695111.1506160772.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); push_noty_num=0; push_doumail_num=0',
 
@@ -64,6 +65,7 @@ UserAgent_List = [
     'ia_archiver (+http://www.alexa.com/site/help/webmasters; crawler@alexa.com)',
 ]
 
+
 def crawer_main():
     while True:
         try:
@@ -72,13 +74,14 @@ def crawer_main():
         except KeyError:
             print('队列没有数据')
         else:
-            data={}
-            data['url']=url
+            data = {}
+            data['url'] = url
             data['id'] = id
             data['star'] = star
             crawer_detail(id, url, data)
 
-def openlink(id,url,data):
+
+def openlink(id, url, data):
     """
     urlopen error 10060错误}
     :param url:  请求的网址
@@ -86,44 +89,47 @@ def openlink(id,url,data):
     :return: 服务器响应
     """
     maxTryNum = 15
-    banIp=[]
+    banIp = []
     for tries in range(maxTryNum):
+        proxies = get_ip3()
         try:
-            proxies=get_ip()
-            while(proxies in banIp):
-                proxies=get_ip3()
-            print('使用代理',proxies)
+            while (proxies in banIp):
+                proxies = get_ip3()
+            # print('使用代理', proxies)
             if not 'm.douban.com' in url:
-                headers['Host']='movie.douban.com'
+                headers['Host'] = 'movie.douban.com'
             else:
-                headers['Host']='m.douban.com'
-            headers['User-Agent']=random.choice(UserAgent_List)
-            headers['Referer']=data['url']
-            time.sleep(0.5)
+                headers['Host'] = 'm.douban.com'
+            headers['User-Agent'] = random.choice(UserAgent_List)
+            headers['Referer'] = data['url']
+            time.sleep(1)
             # proxies={'https': 'https://95.173.194.51:53281'}
-            ipUrl='http://api.ipify.org/'
-            ip_res=requests.get(ipUrl,proxies=proxies,timeout=10)
-            if ip_res.text!='125.88.24.185':
-                print(ip_res.text)
-                if len(CookieList)>0:
-                    res = requests.get(url,headers=headers,proxies=proxies,timeout=10)
-                else:
-                    res=requests.get(url,headers=headers,proxies=proxies,timeout=10)
-                CookieList.append(res.cookies)
-                print(res.status_code)
-                if res.status_code==200:
-                    print("请求成功",url)
-                    return res
-                elif res.status_code==404:
-                    pass
-                    doubanqueue.errorId(id)
-                    break
+            # ipUrl = 'http://api.ipify.org/'
+            # ip_res=requests.get(ipUrl,proxies=proxies,timeout=10)
+            # if ip_res.text!='125.88.24.185':
+            # print(ip_res.text)
+            if len(CookieList) > 0:
+                res = requests.get(url, headers=headers, timeout=10)
+            else:
+                res = requests.get(url, headers=headers, timeout=10)
+            CookieList.append(res.cookies)
+            print(res.status_code)
+            if res.status_code == 200:
+                print("请求成功", url)
+                return res
+            elif res.status_code == 404:
+                pass
+                doubanqueue.errorId(id)
+                break
         except Exception  as e:
             print(e)
+            banIp.append(proxies)
             if tries < (maxTryNum - 1):
                 continue
-            else:
-                print("尝试%d 次连接网址%s失败!" % (maxTryNum, url))
+        else:
+            print("尝试%d 次连接网址%s失败!" % (maxTryNum, url))
+
+
 def get_ip4():
     # 代理服务器
     proxyHost = "http-dyn.abuyun.com"
@@ -145,15 +151,18 @@ def get_ip4():
         # "https": proxyMeta,
     }
     return proxies
+
+
 def get_ip():
     response = requests.get('http://localhost:8899/api/v1/proxies?anonymous=True')
     data = json.loads(response.text)
     proxies = random.choice(data['proxies'])
     if proxies['is_https']:
-        proxy = {'https':'https://' + str(proxies['ip']) + ":" + str(proxies['port'])}
+        proxy = {'https': 'https://' + str(proxies['ip']) + ":" + str(proxies['port'])}
     else:
-        proxy = {'http':'http://' + str(proxies['ip']) + ":" + str(proxies['port'])}
+        proxy = {'http': 'http://' + str(proxies['ip']) + ":" + str(proxies['port'])}
     return proxy
+
 
 def get_ip2():
     r = requests.get('http://127.0.0.1:8000/?types=0&count=5&country=国内')
@@ -165,49 +174,54 @@ def get_ip2():
         'https': 'http://%s:%s' % (ip, port)
     }
     return proxies
+
+
 def get_ip3():
     response = requests.get('http://127.0.0.1:5010/get/')
-    return {'http':'http://'+response.text}
+    return {'http': 'http://' + response.text}
+
 
 def crawer_detail(id, url, data):
-        detailRes = openlink(id,url,data)
-        try:
-            tree = etree.HTML(detailRes.text)
-            name = ''.join(tree.xpath("//div[@id='content']/h1/span[1]/text()"))
-            year = ''.join(tree.xpath("//div[@id='content']/h1/span[2]/text()"))
-            Content = tree.xpath('//div[@class="subject clearfix"]/div[@id="info"]//text()')
-            parse_content(Content, data)
-            rating_num = ''.join(tree.xpath("//strong[@class='ll rating_num']/text()"))
-            rating_people = ''.join(tree.xpath('//a[@class="rating_people"]/span//text()'))
-            rating_item = tree.xpath('//div[@class="ratings-on-weight"]/div')
-            rating_betterthan = '/'.join(list(
-                filter(lambda t: t != '',
-                       map(lambda x: x.strip(), tree.xpath("//div[@class='rating_betterthan']//text()")))))
-            rating_stars = []
-            for weight in rating_item:
-                rating_stars.append(''.join(list(filter(lambda t: t != '', map(lambda x: x.strip(),
-                                                                               weight.xpath(
-                                                                                   './span[1]/text()') + weight.xpath(
-                                                                                   './span[2]/text()'))))))
-            recommendations = tree.xpath("//div[@class='recommendations-bd']/dl")
-            recommend = []
-            for r in recommendations:
-                recommend.append(''.join(r.xpath('./dd/a/text()')))
-            tags = tree.xpath("//div[@class='tags']/div/a/text()")
-            data['name'] = name
-            data['year'] = year
-            data['tags'] = ','.join(tags)
-            data['recommend'] = '/'.join(recommend)
-            data['rating_num'] = rating_num
-            data['rating_people'] = rating_people
-            data['rating_stars'] = '/'.join(rating_stars)
-            data['rating_betterthan'] = rating_betterthan
-            crawer_award(id, data)
-            crawer_topic(id, data)
-            save_database(data)
-            doubanqueue.completeId(id)
-        except Exception as e:
-            print(e)
+    detailRes = openlink(id, url, data)
+    try:
+        tree = etree.HTML(detailRes.text)
+        name = ''.join(tree.xpath("//div[@id='content']/h1/span[1]/text()"))
+        year = ''.join(tree.xpath("//div[@id='content']/h1/span[2]/text()"))
+        Content = tree.xpath('//div[@class="subject clearfix"]/div[@id="info"]//text()')
+        parse_content(Content, data)
+        rating_num = ''.join(tree.xpath("//strong[@class='ll rating_num']/text()"))
+        rating_people = ''.join(tree.xpath('//a[@class="rating_people"]/span//text()'))
+        rating_item = tree.xpath('//div[@class="ratings-on-weight"]/div')
+        rating_betterthan = '/'.join(list(
+            filter(lambda t: t != '',
+                   map(lambda x: x.strip(), tree.xpath("//div[@class='rating_betterthan']//text()")))))
+        rating_stars = []
+        for weight in rating_item:
+            rating_stars.append(''.join(list(filter(lambda t: t != '', map(lambda x: x.strip(),
+                                                                           weight.xpath(
+                                                                               './span[1]/text()') + weight.xpath(
+                                                                               './span[2]/text()'))))))
+        recommendations = tree.xpath("//div[@class='recommendations-bd']/dl")
+        recommend = []
+        for r in recommendations:
+            recommend.append(''.join(r.xpath('./dd/a/text()')))
+        tags = tree.xpath("//div[@class='tags']/div/a/text()")
+        data['name'] = name
+        data['year'] = year
+        data['tags'] = ','.join(tags)
+        data['recommend'] = '/'.join(recommend)
+        data['rating_num'] = rating_num
+        data['rating_people'] = rating_people
+        data['rating_stars'] = '/'.join(rating_stars)
+        data['rating_betterthan'] = rating_betterthan
+        crawer_award(id, data)
+        crawer_topic(id, data)
+        save_database(data)
+        doubanqueue.completeId(id)
+    except Exception as e:
+        print(e)
+
+
 def save_database(data):
     cli = pymongo.MongoClient('123.207.42.164', 27017)
     db = cli['douban']
@@ -216,7 +230,8 @@ def save_database(data):
     except Exception as e:
         print('保存数据错误到数据库')
     else:
-        print('保存数据成功到数据库',data['id'])
+        print('保存数据成功到数据库', data['id'])
+
 
 def parse_content(Content, data):
     valueList = ''.join(Content).split(Content[0])
@@ -230,35 +245,37 @@ def parse_content(Content, data):
                 data[key] = value
 
 
-def crawer_award(id,data):
+def crawer_award(id, data):
     awardUrl = 'https://movie.douban.com/subject/%s/awards/' % id
-    awardRes = openlink(id,awardUrl,data)
+    awardRes = openlink(id, awardUrl, data)
     tree = etree.HTML(awardRes.text)
     awardsList = tree.xpath("//div[@class='article']/div")
     awards_result = {}
     for awards in awardsList:
-        awardName = ''.join(list(filter(lambda t: t != '', map(lambda x: x.strip(),awards.xpath("./div[@class='hd']/h2//text()")))))
+        awardName = ''.join(
+            list(filter(lambda t: t != '', map(lambda x: x.strip(), awards.xpath("./div[@class='hd']/h2//text()")))))
         awards_result[awardName] = list()
         awardList = awards.xpath('.//ul')
         for award in awardList:
             awards_result[awardName].append(
                 ''.join(list(filter(lambda t: t != '', map(lambda x: x.strip(), award.xpath('string(.)'))))))
         awards_result[awardName] = ';'.join(awards_result[awardName])
-    result=[]
-    for key,value in awards_result.items():
-        result.append(key+":"+value)
-    data['awards']=','.join(result)
+    result = []
+    for key, value in awards_result.items():
+        result.append(key + ":" + value)
+    data['awards'] = ','.join(result)
 
 
-def crawer_topic(id,data):
-    topicUrl = 'https://m.douban.com/rexxar/api/v2/gallery/subject_feed?start=0&count=4&subject_id=%s&ck=null'%id
+def crawer_topic(id, data):
+    topicUrl = 'https://m.douban.com/rexxar/api/v2/gallery/subject_feed?start=0&count=4&subject_id=%s&ck=null' % id
     headers['Referer'] = 'https://movie.douban.com/subject/26752088/'
-    topicRes = openlink(id,topicUrl,data)
+    topicRes = openlink(id, topicUrl, data)
     topicData = topicRes.json()
-    topic=[]
+    topic = []
     for i in topicData['items']:
-        topic.append(i['topic']['name']+":"+i['topic']['card_subtitle'])
-    data['topics']='/'.join(topic)
+        topic.append(i['topic']['name'] + ":" + i['topic']['card_subtitle'])
+    data['topics'] = '/'.join(topic)
+
 
 if __name__ == '__main__':
     doubanqueue = MongoQueue('douban', 'url3')
